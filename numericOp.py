d60648462d53d839abc4267efe7f51f3c05e3aba
Karthik Venkatesh <kvenkate@tcd.ie>
2022-09-21 17:26:49 +0100

    test commit

--- numericOp.py
+++ numericOp.py
@@ -1,3 +1,6 @@
 def multiplyOp(n1,n2):
   return n1*n2
 #comment
+
+def addOp(n1,n2):
+  return n1*n1 
--- testing.py
+++ testing.py
@@ -1,5 +1,8 @@
-from numericOp import multiplyOp
+from numericOp import multiplyOp, addOp, mulOp
 
 def testMulti():
   assert multiplyOp(10,11) == 110
 #comment
+
+def testAdd():
+  assert addOp(10,11) == 21
